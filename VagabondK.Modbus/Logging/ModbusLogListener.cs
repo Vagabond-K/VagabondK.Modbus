5efdfbff64f327daa353c6203e7a25f0e43085da
Vagabond K <vagabond-k@naver.com>
2021-02-03 15:55:13 +0900

    로그 리스너 추가

---
+++ VagabondK.Modbus/Logging/ModbusLogListener.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace VagabondK.Modbus.Logging
+{
+    public class ModbusLogListener : IModbusLogger
+    {
+        public ModbusLogCategory CategoryFilter { get; set; }
+        public event EventHandler<ModbusLoggedEventArgs> Logged;
+
+        public void Log(ModbusLog log)
+        {
+            if ((CategoryFilter & log.Category) != 0)
+                Logged?.Invoke(this, new ModbusLoggedEventArgs(log));
+        }
+    }
+
+    public class ModbusLoggedEventArgs : EventArgs
+    {
+        internal ModbusLoggedEventArgs(ModbusLog log)
+        {
+            Log = log;
+        }
+
+        public ModbusLog Log { get; }
+    }
+}
